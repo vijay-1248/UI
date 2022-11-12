79a7dcd796955650e4a187ac1ba1342e743b2c59
vijayasekharpasupuleti <vijaya.sekharpasupuleti@blackboard.com>
2022-11-12 22:18:07 +0530

    objects practice

---
+++ Objects/backpack.js
@@ -0,0 +1,14 @@
+
+const backpack = {
+    name : "every backpack",
+    volumne : 30,
+    color :"gray",
+    pocketName : 15,
+    strapLength : {
+        left : 26 ,
+        right : 26
+    },
+    isLidOpen : false
+}
+
+export default backpack;
---
+++ Objects/index.js
@@ -0,0 +1,2 @@
+import backpack from "./backpack";
+
